dbd17fa5cb683889c742749c1edbd6c59ac38295
judeurban <jude_urban@utulsa.edu>
2021-09-16 09:54:35 -0500

    commit three

--- piSerialCommuncation.py
+++ piSerialCommuncation.py
@@ -6,7 +6,8 @@ if __name__ == '__main__':
     ser.flush()
     idx = 0
     while True:
-        ser.write(b"Hello from Raspberry Pi! ", idx, "\n ")
+        sendString = "Hello from Raspberry Pi! " + idx + "\n"
+        ser.write(sendString.encode('utf-8'))
         line = ser.readline().decode('utf-8').rstrip()
         print(line)
         idx += 1
