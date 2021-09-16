92a0c517c5b46af87defc495d89357be554623e1
judeurban <jude_urban@utulsa.edu>
2021-09-16 09:51:10 -0500

    commit two

--- piSerialCommuncation.py
+++ piSerialCommuncation.py
@@ -4,8 +4,10 @@ import time
 if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
     ser.flush()
+    idx = 0
     while True:
-        ser.write(b"Hello from Raspberry Pi!\n")
+        ser.write(b"Hello from Raspberry Pi! ", idx, "\n ")
         line = ser.readline().decode('utf-8').rstrip()
         print(line)
+        idx += 1
         time.sleep(1)
